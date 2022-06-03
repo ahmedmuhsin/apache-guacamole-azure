e5665cdf0b479cc624770c8ec5ff5f002944c8d2
Ahmed Muhsin <ahmedmuhsin@microsoft.com>
2022-06-03 00:25:41 -0500

    convert comments to console messages

--- setup.sh
+++ setup.sh
@@ -15,10 +15,10 @@ lbguacamolepip=lbguacamolepip
 pipdnsname=loadbalancerguacamole
 lbname=lbguacamole
 
-# Resource Group Creation
+echo -e "\n### Resource Group Creation"
 az group create --name $rg --location $location
 
-# MySQL Creation
+echo -e "\n### MySQL Creation"
 az mysql server create \
     --resource-group $rg \
     --name $mysqldb \
@@ -29,7 +29,7 @@ az mysql server create \
     --storage-size 51200 \
     --ssl-enforcement Disabled
 
-# MySQL Firewall Settings
+echo -e "\n### MySQL Firewall Settings"
 az mysql server firewall-rule create \
     --resource-group $rg \
     --server $mysqldb \
@@ -37,7 +37,7 @@ az mysql server firewall-rule create \
     --start-ip-address 0.0.0.0 \
     --end-ip-address 255.255.255.255
 
-# VNET creation
+echo -e "\n### VNET creation"
 az network vnet create \
     --resource-group $rg \
     --name $vnet \
@@ -45,15 +45,15 @@ az network vnet create \
     --subnet-name $snet \
     --subnet-prefix 10.0.1.0/24
 
-# Availability set creation
+echo -e "\n### Availability set creation"
 az vm availability-set create \
     --resource-group $rg \
     --name $avset \
     --platform-fault-domain-count 2 \
     --platform-update-domain-count 3
 
+echo -e "\n### VMs Creation"
 for i in `seq 1 2`; do
-    # VMs Creation
     az vm create --resource-group $rg \
         --name Guacamole-VM$i \
         --availability-set $avset \
@@ -68,7 +68,7 @@ for i in `seq 1 2`; do
         --nsg $nsg 
     done
 
-# Setting NSG rules
+echo -e "\n### Setting NSG rules"
 az network nsg rule create \
     --resource-group $rg \
     --nsg-name $nsg \
@@ -82,15 +82,15 @@ az network nsg rule create \
     --destination-address-prefix "*" \
     --destination-port-range 80
 
+echo -e "\n### Generating the Guacamole setup script locally on the VMs"
 for i in `seq 1 2`; do
-    # Generating the Guacamole setup script locally on the VMs
     az vm run-command invoke -g $rg -n Guacamole-VM$i  \
         --command-id RunShellScript \
         --scripts "wget https://raw.githubusercontent.com/ahmedmuhsin/apache-guacamole-azure/main/guac-install.sh -O /tmp/guac-install.sh"
     done
     
+echo -e "\n### Adjusting database credentials to match the variables"
 for i in `seq 1 2`; do
-    # Adjusting database credentials to match the variables
     az vm run-command invoke -g $rg -n Guacamole-VM$i  \
         --command-id RunShellScript \
         --scripts "sudo sed -i.bkp -e 's/mysqlpassword/$mysqlpassword/g' \
@@ -98,21 +98,21 @@ for i in `seq 1 2`; do
         -e 's/mysqladmin/$mysqladmin/g' /tmp/guac-install.sh"
     done
 
+echo -e "\n### Executing the Guacamole setup script"
 for i in `seq 1 2`; do
-    # Executing the Guacamole setup script
     az vm run-command invoke -g $rg -n Guacamole-VM$i \
         --command-id RunShellScript \
         --scripts "/bin/bash /tmp/guac-install.sh"
     done
 
+echo -e "\n### Installing Nginx to be used as Proxy for Tomcat"
 for i in `seq 1 2`; do
-    # Installing Nginx to be used as Proxy for Tomcat
     az vm run-command invoke -g $rg -n Guacamole-VM$i \
         --command-id RunShellScript --scripts "sudo apt install --yes nginx-core"
     done
 
+echo -e "\n### Configuring NGINX"
 for i in `seq 1 2`; do
-    # Configuring NGINX
     az vm run-command invoke -g $rg -n Guacamole-VM$i \
         --command-id RunShellScript \
         --scripts "cat <<'EOT' > /etc/nginx/sites-enabled/default
@@ -136,35 +136,35 @@ for i in `seq 1 2`; do
 EOT"
     done
 
+echo -e "\n### Restart NGINX"
 for i in `seq 1 2`; do
-    # Restart NGINX
     az vm run-command invoke -g $rg -n Guacamole-VM$i \
         --command-id RunShellScript \
         --scripts "sudo systemctl restart nginx"
     done
 
+echo -e "\n### Restart Tomcat"
 for i in `seq 1 2`; do
-    # Restart Tomcat
     az vm run-command invoke -g $rg -n Guacamole-VM$i \
         --command-id RunShellScript \
         --scripts "sudo systemctl restart tomcat8"
     done
 
+echo -e "\n### Change to call guacamole directly at "/" instead of "/guacamole""
 for i in `seq 1 2`; do
-    # Change to call guacamole directly at "/" instead of "/guacamole"
     az vm run-command invoke -g $rg -n Guacamole-VM$i \
         --command-id RunShellScript \
         --scripts "sudo /bin/rm -rf /var/lib/tomcat7/webapps/ROOT/* && sudo /bin/cp -pr /var/lib/tomcat8/webapps/guacamole/* /var/lib/tomcat8/webapps/ROOT/"
     done
 
-# Creation of Public IP for the Azure Load Balancer
+echo -e "\n### Creation of Public IP for the Azure Load Balancer"
 az network public-ip create -g $rg -n $lbguacamolepip -l $location \
     --dns-name $pipdnsname \
     --allocation-method static \
     --idle-timeout 4 \
     --sku Standard
 
-# Creation of Azure Load Balancer
+echo -e "\n### Creation of Azure Load Balancer"
 az network lb create -g $rg \
     --name $lbname -l $location \
     --public-ip-address $lbguacamolepip \
@@ -172,7 +172,7 @@ az network lb create -g $rg \
     --frontend-ip-name lbguacafrontend \
     --sku Standard
 
-# Creation of the healthprobe
+echo -e "\n### Creation of the healthprobe"
 az network lb probe create \
     --resource-group $rg \
     --lb-name $lbname \
@@ -182,7 +182,7 @@ az network lb probe create \
     --path / \
     --interval 15 
 
-# Creation of the load balancing rule
+echo -e "\n### Creation of the load balancing rule"
 az network lb rule create \
     --resource-group $rg \
     --lb-name $lbname \
@@ -195,7 +195,7 @@ az network lb rule create \
     --probe-name healthprobe \
     --load-distribution SourceIPProtocol
 
-# Adding VM1 to the Load Balancer
+echo -e "\n### Adding VM1 to the Load Balancer"
 az network nic ip-config update \
     --name ipconfigGuacamole-VM1 \
     --nic-name Guacamole-VM1VMNic \
@@ -203,7 +203,7 @@ az network nic ip-config update \
     --lb-address-pools backendpool \
     --lb-name $lbname 
 
-# Adding VM2 to the Load Balancer
+echo -e "\n### Adding VM2 to the Load Balancer"
 az network nic ip-config update \
     --name ipconfigGuacamole-VM2 \
     --nic-name Guacamole-VM2VMNic \
@@ -211,7 +211,7 @@ az network nic ip-config update \
     --lb-address-pools backendpool \
     --lb-name $lbname 
 
-# Creating the INAT Rules
+echo -e "\n### Creating the INAT Rules"
 az network lb inbound-nat-rule create \
     --resource-group $rg \
     --lb-name $lbname \
